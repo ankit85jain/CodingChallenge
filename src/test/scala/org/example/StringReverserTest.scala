c3d4ca19df909f0b54f7cdd05c1defdff8e577c7
ankit85jain <55641872+ankit85jain@users.noreply.github.com>
2020-02-08 00:09:13 +0530

    Add files via upload

---
+++ src/test/scala/org/example/StringReverserTest.scala
@@ -0,0 +1,26 @@
+package org.example
+import org.junit._
+import org.junit.Assert._
+@Test
+class StringReverserTest {
+
+  @Test
+  def testReverse1() = {
+    assertEquals("madA m'I madaM", StringReverser.reverse1("Madam I'm Adam"))
+  }
+
+  @Test
+  def testReverse2() = {
+    assertEquals("madA m'I madaM", StringReverser.reverse2("Madam I'm Adam"))
+  }
+
+  @Test
+  def testReverse3() = {
+    assertEquals("madA m'I madaM", StringReverser.reverse3("Madam I'm Adam"))
+  }
+
+  @Test
+  def testReverse4() = {
+    assertEquals("madA m'I madaM", StringReverser.reverse4("Madam I'm Adam"))
+  }
+}
